124e69b18c01f01c82717e155c9a277d3bbd8706
Kai Polster-Ewing <kailiamewing@gmail.com>
2020-03-16 10:32:08 +1100

    Init

--- MathMethodsDiscordBot/Program.cs
+++ MathMethodsDiscordBot/Program.cs
@@ -6,7 +6,7 @@ namespace MathMethodsDiscordBot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
