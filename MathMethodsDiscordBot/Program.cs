abc2aeea7c210d12901e560e4a8c489c2ec98aa4
Kai Polster-Ewing <kailiamewing@gmail.com>
2020-03-16 10:23:36 +1100

    Add project files.

---
+++ MathMethodsDiscordBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathMethodsDiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
